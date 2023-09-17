d74cedb120634bb8e36c420c323909c82af5acbc
Dominik Procházka <dominikprochazka100@gmail.com>
2023-09-17 20:54:14 +0200

    panther/cheetah: Init neoteric

--- neoteric_cheetah.mk
+++ neoteric_cheetah.mk
@@ -6,18 +6,18 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/neoteric/target/product/neoteric-target.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/pantah/aosp_cheetah.mk)
-$(call inherit-product, device/google/gs201/lineage_common.mk)
+$(call inherit-product, device/google/gs201/neoteric_common.mk)
 
-include device/google/pantah/cheetah/device-lineage.mk
+include device/google/pantah/cheetah/device-neoteric.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7 Pro
-PRODUCT_NAME := lineage_cheetah
+PRODUCT_NAME := neoteric_cheetah
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3120
