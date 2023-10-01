62de29b5942853d5b8e5d5f441eb31eb09d25ccf
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-01 22:12:08 +0200

    panther/cheetah: Set frame rate multiple threshold

--- device-panther.mk
+++ device-panther.mk
@@ -61,6 +61,9 @@ PRODUCT_COPY_FILES += \
 # Media Performance Class 13
 PRODUCT_PROPERTY_OVERRIDES += ro.odm.build.media_performance_class=33
 
+#Display
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += debug.sf.frame_rate_multiple_threshold=90
+
 # Display Config
 PRODUCT_COPY_FILES += \
         device/google/pantah/panther/display_colordata_dev_cal0.pb:$(TARGET_COPY_OUT_VENDOR)/etc/display_colordata_dev_cal0.pb \
